a0ba76c86b1f739fd65b5b0080eb3f4f187b6971
jaha7 <47249045+jaha7@users.noreply.github.com>
2020-04-24 15:08:54 +0200

    Change pin/marker icon

--- src/style/pin.js
+++ src/style/pin.js
@@ -3,7 +3,7 @@ const pin = [{
     anchor: [0.5, 32],
     anchorXUnits: 'fraction',
     anchorYUnits: 'pixels',
-    src: 'img/png/drop_blue.png'
+    src: 'img/png/falk_marker_red.png'
   }
 }];
 
