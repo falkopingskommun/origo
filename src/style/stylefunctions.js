ab18f3b62963cd1f0d0e293ab9faac654f3e5d88
David Johansson <david.a.johansson@falkoping.se>
2021-06-10 14:58:40 +0200

    New custom style

--- src/style/stylefunctions.js
+++ src/style/stylefunctions.js
@@ -2,12 +2,14 @@ import defaultStyle from './stylefunctions/default';
 import detaljplanStyle from './stylefunctions/falk_dp_hatch';
 import prickmarkStyle from './stylefunctions/falk_dp_hatch_prickmark';
 import korsmarkStyle from './stylefunctions/falk_dp_hatch_korsmark';
+import op_natur from './stylefunctions/falk_op_natur_hatch';
 
 const customStyles = {
   default: defaultStyle,
   falk_dp_hatch: detaljplanStyle,
   falk_dp_hatch_prickmark: prickmarkStyle,
-  falk_dp_hatch_korsmark: korsmarkStyle
+  falk_dp_hatch_korsmark: korsmarkStyle,
+  falk_op_natur_hatch: op_natur,
 };
 
 export default function styleFunctions(customStyle, params) {
