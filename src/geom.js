4976bc2e5c699a85ef40251dd940049931a5dfdc
David Johansson <david.a.johansson@falkoping.se>
2022-04-12 16:02:51 +0200

    area_ha: new area option for selected feature
    area_ha: new area option for selected feature

--- src/geom.js
+++ src/geom.js
@@ -1,8 +1,10 @@
 import getcenter from './geometry/getcenter';
 import getarea from './geometry/getarea';
+import getarea_ha from './geometry/getarea_ha';
 
 const geom = {};
 geom.center = getcenter;
 geom.area = getarea;
+geom.area_ha = getarea_ha;
 
 export default geom;
