d3cc8596641b024e49cfeca11a26c1cd67dcc59a
David Johansson <david.a.johansson@falkoping.se>
2022-04-12 16:17:46 +0200

    Correction: getarea_ha

--- src/geometry/getarea_ha.js
+++ src/geometry/getarea_ha.js
@@ -2,7 +2,6 @@ import round from '../utils/round';
 
 export default function getArea_ha(geometryIn, decimals) {
   let area_ha = geometryIn.getArea ? geometryIn.getArea() : 0;
-  console.log("1", decimals)
   if (decimals) {
     area_ha = round(area_ha/10000, decimals);
   } else {
