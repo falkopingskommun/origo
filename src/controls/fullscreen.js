0138f10cdfa15ad77742c652cb732fbed164c417
Pär Owemyr <par.ovemyr@falkoping.se>
2022-07-08 11:15:55 +0200

    Fixed ugly fullscreen btn

--- src/controls/fullscreen.js
+++ src/controls/fullscreen.js
@@ -23,7 +23,7 @@ const Fullscreen = function Fullscreen(options = {}) {
     onAdd(evt) {
       viewer = evt.target;
       fullscreenButton = Button({
-        cls: 'o-fullscreen padding-small icon-smaller round light box-shadow falk-embed',
+        cls: 'o-fullscreen padding-small icon-smaller light box-shadow falk-embed', //FM
         text: 'Visa stor karta',
         click() {
           goFullScreen();
